55f0948146afe56d83ede3834265cc97be8d39d4
gritz <gritz@DESKTOP>
2022-08-30 18:23:21 +0930

    edit on the index and style

--- Home/index.php
+++ Home/index.php
@@ -9,8 +9,28 @@
 </head>
 
 <body>
+<div class="top_third">
+    <div class="menu_container">
+        <div class="nav">
+            <h1>S E N I O R</h1>
+            <ul class="main_menu">
+                <li><a href="#"><span class="media_text">Home</span></a></li>
+                <li><a href="#"><span class="media_text">Community</span></a></li>
+                <li><a href="#"><span class="media_text">Marketplace</span></a></li>
+                <li><a href="#"><span class="media_text">Register</span></a></li>
+                <li><a href="#"><span class="media_text">Login</span></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
 
-S.E.N.I.O.R
+<div class="page_wrapper">
+
+    <div class="home_body">
+
+    </div>
+
+</div>
 
 </body>
 
