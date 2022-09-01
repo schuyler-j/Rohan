9e8a962bce6a1f100ab6a32e2f2c717f42be76b6
gritz <gritz@DESKTOP>
2022-09-01 13:57:01 +0930

    changed register to about

--- index.php
+++ index.php
@@ -18,12 +18,13 @@
             <span>GREY</span> <span id="alt">NOMADS</span> <span>INTERESTED</span> <span id="alt">IN</span> <span>OUTDOOR</span> <span id="alt">RECREATION</span>
         </div>
     <div class="menu_container">
+        <h1 id="menu_title_s">SENIOR</h1>
         <div class="nav">
             <ul class="main_menu">
-                <li class="list"><a href="#" id="home"><span class="media_text">Home</span></a></li>
+                <li class="list"><a href="#" id="selected"><span class="media_text">Home</span></a></li>
                 <li class="list"><a href="#"><span class="media_text">Community</span></a></li>
                 <li class="list"><a href="#"><span class="media_text">Marketplace</span></a></li>
-                <li class="list"><a href="#"><span class="media_text">Register</span></a></li>
+                <li class="list"><a href="#"><span class="media_text">About</span></a></li>
                 <li class="list"><a href="#"><span class="media_text">Login</span></a></li>
             </ul>
         </div>
@@ -32,8 +33,16 @@
 
 <div class="page_wrapper">
     <div class="home_body">
+        <div class="grid">
+            <div class="block_1">
+                <img src="images/greeting_1.png">
+            </div>
+            <div class="block_1">
+                <img src="images/greeting_2.png">
+            </div>
+        </div>
         <div class="home_body_text">
-            <span>Join the Community Today!</span>
+            <input type="submit" value="JOIN NOW" id="button"></input>
         </div>
     </div>
 </div>
