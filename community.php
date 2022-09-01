17772191bb006957f96e10460a7050b4664df503
gritz <gritz@DESKTOP>
2022-09-01 19:05:06 +0930

    added new graphic

--- community.php
+++ community.php
@@ -17,8 +17,8 @@
         <h1 id="menu_title_s">SENIOR</h1>
         <div class="nav">
             <ul class="main_menu">
-                <li class="list"><a href="#" id="selected"><span class="media_text">Home</span></a></li>
-                <li class="list"><a href="community.php"><span class="media_text">Community</span></a></li>
+                <li class="list"><a href="index.php"><span class="media_text">Home</span></a></li>
+                <li class="list"><a href="#" id="selected"><span class="media_text">Community</span></a></li>
                 <li class="list"><a href="#"><span class="media_text">Marketplace</span></a></li>
                 <li class="list"><a href="#"><span class="media_text">About</span></a></li>
                 <li class="list"><a href="#"><span class="media_text">Login</span></a></li>
@@ -27,7 +27,7 @@
     </div>
 </div>
 
-<div class="page_wrapper">
+<div class="page_wrapper_c">
     <div class="side_navbar">
         <div class="nav_title"><h2>Recent News</h2></div>
         <div class="link_box">
@@ -40,24 +40,6 @@
             <a href="#">Item 1</a>
         </div>
 
-    </div>
-    <div class="home_body">
-        <div class="title">
-            <span>SHOPPING</span> <span id="alt">AND</span> <span>EXCHANGE</span> <span id="alt">FOR</span> 
-            <span>GREY</span> <span id="alt">NOMADS</span> <span>INTERESTED</span> <span id="alt">IN</span> <span>OUTDOOR</span> <span id="alt">RECREATION</span>
-        </div>
-        <div class="home_body_text">
-            <span>Ipsum elit ad sint anim. </span>
-            <span>Velit sint qui ipsum amet ex cupidatat minim non sunt esse enim. </span>
-            <span>Tempor fugiat voluptate eiusmod dolore eu irure elit.</span> 
-        </div>
-        <div class="grid">
-            <div class="block_1">
-                <img src="images/greeting_2.png">
-                <input type="submit" value="JOIN NOW" id="button"></input>
-            </div>
-                <img id="cursor" src="images/cursor.png">
-        </div>
     </div>
 </div>
 </body>
