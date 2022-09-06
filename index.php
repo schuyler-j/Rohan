7237b87b14899e0089e54c3d334eeaeeaa409135
gritz <gritz@DESKTOP>
2022-09-06 16:15:36 +0930

    buttons and background

--- index.php
+++ index.php
@@ -26,27 +26,27 @@
         </a>
         -->
     </div>
-        <div class="nav" id="nav_bottom">
-            <div class="nav_list">
-                <img src="images/watchlist.png">
-                <a class="nav_links" href="#">Watchlist</a>
-                <img src="images/cart.png">
-                <a class="nav_links" href="#">My Cart</a>
-                <img src="images/checkout.png">
-                <a class="nav_links" href="#">Checkout</a>
-                <img src="images/login.png">
-                <a class="nav_links" href="#">Login</a>
-            </div>
+    <div class="nav" id="nav_bottom">
+        <div class="nav_list">
+            <img src="images/watchlist.png">
+            <a class="nav_links" href="#">Watchlist</a>
+            <img src="images/cart.png">
+            <a class="nav_links" href="#">My Cart</a>
+            <img src="images/checkout.png">
+            <a class="nav_links" href="#">Checkout</a>
+            <img src="images/login.png">
+            <a class="nav_links" href="#">Login</a>
         </div>
-        <div class="nav" id="nav_top">
-            <ul class="main_menu">
-                <li class="list"><a href="#" id="selected"><span class="media_text">Home</span></a></li>
-                <li class="list"><a href="community.php"><span class="media_text">Community Marketplace</span></a></li>
-                <li class="list"><a href="#"><span class="media_text">Shopping</span></a></li>
-                <li class="list"><a href="#about"><span class="media_text">About</span></a></li>
-                <li class="list"><a href="#"><span class="media_text">Contact</span></a></li>
-            </ul>
-        </div>
+    </div>
+    <div class="nav" id="nav_top">
+        <ul class="main_menu">
+            <li class="list"><a href="#" id="selected"><span class="media_text">Home</span></a></li>
+            <li class="list"><a href="community.php"><span class="media_text">Community Marketplace</span></a></li>
+            <li class="list"><a href="#"><span class="media_text">Shopping</span></a></li>
+            <li class="list"><a href="#about"><span class="media_text">About</span></a></li>
+            <li class="list"><a href="#"><span class="media_text">Contact</span></a></li>
+        </ul>
+    </div>
 </div>
 
 <div class="page_wrapper">
