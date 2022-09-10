f58363236d4a7f62395e1d637ead5851f7b65c60
Udall8434 <liao0108@flinders.edu.au>
2022-09-10 12:35:34 +0930

    Added 'about'

--- about.php
+++ about.php
@@ -11,7 +11,31 @@
 </head>
 
 <body>
+    </div>
+        <div class="nav" id="nav_bottom">
+            <div class="nav_list">
+                <img src="images/watchlist.png">
+                <a class="nav_links" href="#">Watchlist</a>
+                <img src="images/cart.png">
+                <a class="nav_links" href="#">My Cart</a>
+                <img src="images/checkout.png">
+                <a class="nav_links" href="#">Checkout</a>
+                <img src="images/login.png">
+                <a class="nav_links" href="#">Login</a>
+            </div>
+        </div>
+        <div class="nav" id="nav_top">
+            <ul class="main_menu">
+                <li class="list"><a href="index.php"><span class="media_text">Home</span></a></li>
+                <li class="list"><a href="#" id="selected"><span class="media_text">Community Marketplace</span></a></li>
+                <li class="list"><a href="#"><span class="media_text">Shopping</span></a></li>
+                <li class="list"><a href="#"><span class="media_text">About</span></a></li>
+                <li class="list"><a href="#"><span class="media_text">Contact</span></a></li>
+            </ul>
+        </div>
+</div>
+
     <div>
         
-    </div>
+</div>
 </body>
