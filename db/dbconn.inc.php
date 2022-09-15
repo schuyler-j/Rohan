ca0f5b6a3b692c2f323b4803bef4f7571a6e9c95
Jay <schu0527@flinders.edu.au>
2022-09-15 20:12:44 +0930

    db up

--- db/dbconn.inc.php
+++ db/dbconn.inc.php
@@ -1,9 +1,11 @@
 <?php
 
+require_once "db/secret.inc.php";
+
 define("DB_HOST", "3.27.8.66");
 define("DB_NAME", "ecom");
 define("DB_USER", "ec2-user");
-define("DB_PASS", "hlriirwbzm");
+define("DB_PASS", $secret);
 
 $conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if (!$conn) {
