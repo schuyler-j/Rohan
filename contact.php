6ffe57e2df600b34317a3c83fec4f6e7ba00d59b
Udall8434 <liao0108@flinders.edu.au>
2022-09-15 20:28:00 +0930

    Added contact page

---
+++ contact.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>About</title>
+<meta charset="UTF-8" />
+<meta name="author" content="TUJ_Rohan" />
+<link rel="stylesheet" href="styles/style.css" />
+<!-- <link rel="stylesheet" href="styles/about.css" /> -->
+<link rel="icon" href="images/favicon.png">
+<script src="scripts/script.js" defer></script>
+</head>
+
+<body>
+<div class="top_third">
+    <div class="menu_container">
+        <h1 class="menu_title_s">SENIOR</h1>
+        <!--
+        <div class="menu_title_s" id="logo">
+            <img src="images/home_banner.png" width="40%">
+        </div>
+        -->
+    </div>
+        <div class="nav" id="nav_bottom">
+            <div class="nav_list">
+                <img src="images/watchlist.png">
+                <a class="nav_links" href="#">Watchlist</a>
+                <img src="images/cart.png">
+                <a class="nav_links" href="#">My Cart</a>
+                <img src="images/checkout.png">
+                <a class="nav_links" href="#">Checkout</a>
+                <img src="images/login.png">
+                <a class="nav_links" href="#">Login</a>
+            </div>
+        </div>
+        <div class="nav" id="nav_top">
+            <ul class="main_menu">
+                <li class="list"><a href="index.php"><span class="media_text">Home</span></a></li>
+                <li class="list"><a href="community.php"><span class="media_text">Community Marketplace</span></a></li>
+                <li class="list"><a href="shopping.php"><span class="media_text">Shopping</span></a></li>
+                <li class="list"><a href="about.php"><span class="media_text">About</span></a></li>
+                <li class="list"><a href="#" id="selected"><span class="media_text">Contact</span></a></li>
+            </ul>
+        </div>
+</div>
+
+    <div class = "title">
+        <h2>Contact Us</h2>
+            <p>
+                <span>This is a test to see how the contact page works </span>
+
+            </p>
+</div>
+</body>
