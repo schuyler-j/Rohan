2e9b2add9db27a692a737f75db65770b1e0a79c6
Jay <74050386+lefth-nd@users.noreply.github.com>
2022-09-19 20:12:32 +0930

    Create checkout.php

---
+++ checkout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Cart</title>
+<meta charset="UTF-8" />
+<meta name="author" content="TUJ_Rohan" />
+<link rel="stylesheet" href="styles/style.css" />
+<link rel="stylesheet" href="styles/footer.css" />
+<link rel="icon" href="images/favicon.png">
+<script src="scripts/script.js" defer></script>
+</head>
+
+<body>
+</body>
+  
+</html>
