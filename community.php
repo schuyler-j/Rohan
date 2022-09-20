2a86eafe3207e2b98893383b452c91f235651c81
Jay <schu0527@flinders.edu.au>
2022-09-20 14:31:31 +0930

    php updates

--- community.php
+++ community.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<title>Community</title>
-<meta charset="UTF-8" />
-<meta name="author" content="Team_Rohan" />
-<link rel="stylesheet" href="styles/style.css" />
-<link rel="stylesheet" href="styles/community.css" />
-<link rel="icon" href="images/favicon.png">
-<script src="scripts/script.js" defer></script>
+    <title>Community</title>
+    <meta charset="UTF-8" />
+    <meta name="author" content="Team_Rohan" />
+    <link rel="stylesheet" href="styles/style.css" />
+    <link rel="stylesheet" href="styles/community.css" />
+    <link rel="icon" href="images/favicon.png">
+    <script src="scripts/script.js" defer></script>
 </head>
-<?php 
-require_once "db/dbconn.inc.php"; 
+<?php
+require_once "db/dbconn.inc.php";
 require_once "addcart.php";
 
 
@@ -19,22 +20,22 @@ session_start();
 ?>
 
 <body>
-<div class="top_third">
-    <div class="menu_container">
-        <h1 class="menu_title_s"><a href="index.php">SENIOR</a></h1>
-    </div>
+    <div class="top_third">
+        <div class="menu_container">
+            <h1 class="menu_title_s"><a href="index.php">SENIOR</a></h1>
+        </div>
 
-    <div class="nav" id="nav_bottom">
-        <div class="nav_list">
-            <img src="images/watchlist.png"/>
-            <a class="nav_links" href="#">Watchlist</a>
-            <img src="images/cart.png"/>
-            <a class="nav_links" href="#">My Cart</a>
-            <img src="images/checkout.png"/>
-            <a class="nav_links" href="#">Checkout</a>
-            <img src="images/login.png"/>
-                <?php 
-                if(isset($_SESSION["active"]) && $_SESSION["active"] === true){
+        <div class="nav" id="nav_bottom">
+            <div class="nav_list">
+                <img src="images/watchlist.png" />
+                <a class="nav_links" href="#">Watchlist</a>
+                <img src="images/cart.png" />
+                <a class="nav_links" href="#">My Cart</a>
+                <img src="images/checkout.png" />
+                <a class="nav_links" href="#">Checkout</a>
+                <img src="images/login.png" />
+                <?php
+                if (isset($_SESSION["active"]) && $_SESSION["active"] === true) {
                     echo "<a class = 'nav_links' href='logout.php'>Logout</a>";
 
                     $product_sql = "SELECT * FROM `products` WHERE cItem = 0;";
@@ -51,69 +52,68 @@ session_start();
                     $userid = $_SESSION["id"];
 
                     $productid = 0;
-
-
-                }else{
-                    echo 
+                } else {
+                    echo
                     "<a class = 'nav_links' href='login.php'>Login</a>
                     ";
 
                     $addcart = 'error.php';
                     $addwish = 'error.php';
                     $msg = "?msg=Please%20login%20or%20create%20an%20account.";
-
                 }
                 ?>
+            </div>
+        </div>
+        <div class="nav" id="nav_top">
+            <ul class="main_menu">
+                <li class="list"><a href="index.php"><span class="media_text">Home</span></a></li>
+                <li class="list"><a href="#" id="selected"><span class="media_text">Community Marketplace</span></a></li>
+                <li class="list"><a href="shopping.php"><span class="media_text">Shopping</span></a></li>
+                <li class="list"><a href="about.php"><span class="media_text">About</span></a></li>
+                <li class="list"><a href="contact.php"><span class="media_text">Contact</span></a></li>
+            </ul>
         </div>
     </div>
-    <div class="nav" id="nav_top">
-        <ul class="main_menu">
-            <li class="list"><a href="index.php"><span class="media_text">Home</span></a></li>
-            <li class="list"><a href="#" id="selected"><span class="media_text">Community Marketplace</span></a></li>
-            <li class="list"><a href="shopping.php"><span class="media_text">Shopping</span></a></li>
-            <li class="list"><a href="about.php"><span class="media_text">About</span></a></li>
-            <li class="list"><a href="contact.php"><span class="media_text">Contact</span></a></li>
-        </ul>
-    </div>
-</div>
-<div class="page_wrapper">
-    <div class="side_navbar">
-        <div class="nav_title"><h2>Community Items</h2></div>
-        <div class="link_box">
-            <?php 
+    <div class="page_wrapper">
+        <div class="side_navbar">
+            <div class="nav_title">
+                <h2>Community Items</h2>
+            </div>
+            <div class="link_box">
+                <?php
 
 
-            if(isset($_GET["action"]) && $_GET["action"] == "ac"){
-                $productid = $_GET["id"];
-                $cart_sql = "INSERT INTO `cart` (`CartID`, `TotalPrice`, `Shipping`, `Payment`, `DoP`, `UserID`, `ProductID`, `CreditCard`) VALUES (CONNECTION_ID(), $total, '$addr', 'credit', CURRENT_DATE(), $userid, $productid, $creditcard);";
-                $update_stock = "UPDATE `products` SET `stockAmt` = (stockAmt - 1) WHERE `products`.`ProductID` = $productid;";
-                $statement = mysqli_stmt_init($conn);
-                $update_st = mysqli_stmt_init($conn);
-                mysqli_stmt_prepare($statement, $cart_sql);
-                mysqli_stmt_prepare($update_st, $update_stock);
-                mysqli_stmt_execute($statement);
-                mysqli_stmt_execute($update_st);
-            }
-            
-            $product_sql = "SELECT * FROM `products` WHERE cItem = 0 AND stockAmt > 0;";
-            if($prod_result = mysqli_query($conn, $product_sql)){
-                if(mysqli_num_rows($prod_result) > 0){
-                    while($row = mysqli_fetch_assoc($prod_result)){
-                        $title = $row["pName"];
-                        $img = $row["imgSrc"];
-                        $desc = $row["Description"];
-                        $price = $row["Price"];
+                if (isset($_GET["action"]) && $_GET["action"] == "ac") {
+                    $productid = $_GET["id"];
+                    $cart_sql = "INSERT INTO `cart` (`CartID`, `TotalPrice`, `Shipping`, `Payment`, `DoP`, `UserID`, `ProductID`, `CreditCard`) VALUES (CONNECTION_ID(), $total, '$addr', 'credit', CURRENT_DATE(), $userid, $productid, $creditcard);";
+                    $update_stock = "UPDATE `products` SET `stockAmt` = (stockAmt - 1) WHERE `products`.`ProductID` = $productid;";
+                    $statement = mysqli_stmt_init($conn);
+                    $update_st = mysqli_stmt_init($conn);
+                    mysqli_stmt_prepare($statement, $cart_sql);
+                    mysqli_stmt_prepare($update_st, $update_stock);
+                    mysqli_stmt_execute($statement);
+                    mysqli_stmt_execute($update_st);
+                }
+
+                $product_sql = "SELECT * FROM `products` WHERE cItem = 0 AND stockAmt > 0;";
+                if ($prod_result = mysqli_query($conn, $product_sql)) {
+                    if (mysqli_num_rows($prod_result) > 0) {
+                        while ($row = mysqli_fetch_assoc($prod_result)) {
+                            $title = $row["pName"];
+                            $img = $row["imgSrc"];
+                            $desc = $row["Description"];
+                            $price = $row["Price"];
 
 
-                        if(isset($_SESSION["active"]) && $_SESSION["active"] === true){
-                            $addcart = 'community.php?action=ac&id=' . $row["ProductID"];
-                            $addwish = 'community.php?action=aw&id=' . $row["ProductID"];
-                        }
+                            if (isset($_SESSION["active"]) && $_SESSION["active"] === true) {
+                                $addcart = 'community.php?action=ac&id=' . $row["ProductID"];
+                                $addwish = 'community.php?action=aw&id=' . $row["ProductID"];
+                            }
 
-                        echo "
+                            echo "
                         <div>
                             <div class='sub_heading'>
-                                <h4>". $title . "</h4>
+                                <h4>" . $title . "</h4>
                             </div>
                         </div>
                         <div class='img_container'><a href='landing.php'><img src='images/$img'/></a></div>
@@ -136,39 +136,40 @@ session_start();
                         </div>
 
                         ";
-
+                        }
                     }
                 }
-            }
 
-            ?>
+                ?>
+            </div>
         </div>
-    </div>
-    <div class="h_spacer"></div>
-    <div class="side_navbar">
-        <div class="nav_title"><h2>On Sale!</h2></div>
-        <div class="link_box">
-            <?php 
-            
-            $product_sql = "SELECT * FROM `products` WHERE cItem = 1 AND onSale = 1 AND stockAmt > 0;";
-            if($prod_result = mysqli_query($conn, $product_sql)){
-                if(mysqli_num_rows($prod_result) > 0){
-                    while($row = mysqli_fetch_assoc($prod_result)){
-                        $title = $row["pName"];
-                        $img = $row["imgSrc"];
-                        $desc = $row["Description"];
-                        $price = $row["Price"];
-                        $salePrice = $row["salePrice"];
+        <div class="h_spacer"></div>
+        <div class="side_navbar">
+            <div class="nav_title">
+                <h2>On Sale!</h2>
+            </div>
+            <div class="link_box">
+                <?php
 
-                        if(isset($_SESSION["active"]) && $_SESSION["active"] === true){
-                            $addcart = 'community.php?action=ac&id=' . $row["ProductID"];
-                            $addwish = 'community.php?action=aw&id=' . $row["ProductID"];
-                        }
+                $product_sql = "SELECT * FROM `products` WHERE cItem = 1 AND onSale = 1 AND stockAmt > 0;";
+                if ($prod_result = mysqli_query($conn, $product_sql)) {
+                    if (mysqli_num_rows($prod_result) > 0) {
+                        while ($row = mysqli_fetch_assoc($prod_result)) {
+                            $title = $row["pName"];
+                            $img = $row["imgSrc"];
+                            $desc = $row["Description"];
+                            $price = $row["Price"];
+                            $salePrice = $row["salePrice"];
 
-                        echo "
+                            if (isset($_SESSION["active"]) && $_SESSION["active"] === true) {
+                                $addcart = 'community.php?action=ac&id=' . $row["ProductID"];
+                                $addwish = 'community.php?action=aw&id=' . $row["ProductID"];
+                            }
+
+                            echo "
                         <div>
                             <div class='sub_heading'>
-                                <h4>". $title . "</h4>
+                                <h4>" . $title . "</h4>
                             </div>
                         </div>
                         <div class='img_container'><a href='landing.php'><img src='images/$img'/></a></div>
@@ -197,40 +198,41 @@ session_start();
                         </div>
 
                         ";
-
+                        }
                     }
                 }
-            }
 
-            ?>
+                ?>
 
 
+            </div>
         </div>
-    </div>
-    <div class="h_spacer"></div>
-    <div class="side_navbar">
-        <div class="nav_title"><h2>Featured Items</h2></div>
-        <div class="link_box">
-            <?php 
-            
-            $product_sql = "SELECT * FROM `products` WHERE cItem = 1 AND featured = 1 AND stockAmt > 0;";
-            if($prod_result = mysqli_query($conn, $product_sql)){
-                if(mysqli_num_rows($prod_result) > 0){
-                    while($row = mysqli_fetch_assoc($prod_result)){
-                        $title = $row["pName"];
-                        $img = $row["imgSrc"];
-                        $desc = $row["Description"];
-                        $price = $row["Price"];
+        <div class="h_spacer"></div>
+        <div class="side_navbar">
+            <div class="nav_title">
+                <h2>Featured Items</h2>
+            </div>
+            <div class="link_box">
+                <?php
 
-                        if(isset($_SESSION["active"]) && $_SESSION["active"] === true){
-                            $addcart = 'community.php?action=ac&id=' . $row["ProductID"];
-                            $addwish = 'community.php?action=aw&id=' . $row["ProductID"];
-                        }
+                $product_sql = "SELECT * FROM `products` WHERE cItem = 1 AND featured = 1 AND stockAmt > 0;";
+                if ($prod_result = mysqli_query($conn, $product_sql)) {
+                    if (mysqli_num_rows($prod_result) > 0) {
+                        while ($row = mysqli_fetch_assoc($prod_result)) {
+                            $title = $row["pName"];
+                            $img = $row["imgSrc"];
+                            $desc = $row["Description"];
+                            $price = $row["Price"];
 
-                        echo "
+                            if (isset($_SESSION["active"]) && $_SESSION["active"] === true) {
+                                $addcart = 'community.php?action=ac&id=' . $row["ProductID"];
+                                $addwish = 'community.php?action=aw&id=' . $row["ProductID"];
+                            }
+
+                            echo "
                         <div>
                             <div class='sub_heading'>
-                                <h4>". $title . "</h4>
+                                <h4>" . $title . "</h4>
                             </div>
                         </div>
                         <div class='img_container' id='featured'><a href='landing.php'><img src='images/$img'/></a></div>
@@ -253,21 +255,21 @@ session_start();
                         </div>
 
                         ";
-
+                        }
                     }
                 }
-            }
 
-            ?>
+                ?>
 
+            </div>
         </div>
     </div>
-</div>
-<div class="pages" style="text-align: center;">
-    <a href="community.php">^ Back to Top ^</a>
-</div>
-<div class="footer">
-    <h4>Thomas Hobbs | Udall Liao | Jay Schuyler</h4>
-</div>
+    <div class="pages" style="text-align: center;">
+        <a href="community.php">^ Back to Top ^</a>
+    </div>
+    <div class="footer">
+        <h4>Thomas Hobbs | Udall Liao | Jay Schuyler</h4>
+    </div>
 </body>
+
 </html>
