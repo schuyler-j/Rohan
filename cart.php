6ed779043e39b0081ffb5b542cba8b8ddbf09ee0
Hobb0097 <hobb0097@flinders.edu.au>
2022-09-21 20:32:26 +0930

    cart page

---
+++ cart.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Cart</title>
+<meta charset="UTF-8" />
+<meta name="author" content="TUJ_Rohan" />
+<link rel="stylesheet" href="styles/style.css" />
+<link rel="stylesheet" href="styles/footer.css" />
+<link rel="icon" href="images/favicon.png">
+<script src="scripts/script.js" defer></script>
+</head>
+
+<body>
+<div class="top_third">
+    <div class="menu_container">
+        <h1 class="menu_title_s">SENIOR</h1>
+        <!--
+        <div class="menu_title_s" id="logo">
+            <img src="images/home_banner.png" width="40%">
+        </div>
+        -->
+    </div>
+        <div class="nav" id="nav_bottom">
+            <div class="nav_list">
+                <img src="images/watchlist.png">
+                <a class="nav_links" href="#">Watchlist</a>
+                <img src="images/cart.png">
+                <a class="nav_links" href="#">My Cart</a>
+                <img src="images/checkout.png">
+                <a class="nav_links" href="#">Checkout</a>
+                <img src="images/login.png">
+                <a class="nav_links" href="#">Login</a>
+            </div>
+        </div>
+        <div class="nav" id="nav_top">
+            <ul class="main_menu">
+                <li class="list"><a href="index.php"><span class="media_text">Home</span></a></li>
+                <li class="list"><a href="community.php"><span class="media_text">Community Marketplace</span></a></li>
+                <li class="list"><a href="shopping.php"><span class="media_text">Shopping</span></a></li>
+                <li class="list"><a href="about.php" id="selected"><span class="media_text">About</span></a></li>
+                <li class="list"><a href="contact.php"><span class="media_text">Contact</span></a></li>
+            </ul>
+        </div>
+</div>
+    <div class = "title">
+        <h2>My Cart</h2>
+        <div>
+        </div>
+    </div>
+</body>  
+</html>
