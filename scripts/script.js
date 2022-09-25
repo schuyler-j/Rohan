dfaa813f5db242f30ccab296e9048fc9dd0ed11d
Jay <schu0527@flinders.edu.au>
2022-09-25 14:25:39 +0930

    script to generate bg

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,13 @@ function ShowPassword() {
     }
 
 }
+
+var bg = ['images/home_banner.png', 'images/home_banner_2.png'];
+function BackgroundRefresh() {
+    num = Math.floor(Math.random()*bg.length);
+    const menu_cont = document.getElementById("mc");
+
+    menu_cont.setAttribute('style', 'background-image: url('+(bg[num])+')');
+}
+
+BackgroundRefresh();
