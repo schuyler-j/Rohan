89fa7a345d67fba5cb6340923e33a393498cf6df
Hobb0097 <hobb0097@flinders.edu.au>
2022-09-30 16:36:59 +0930

    landing styling

--- landing.php
+++ landing.php
@@ -59,9 +59,15 @@ session_start();
         <ul class="item_list" id="cart_form">
                     <li><div class="sub_heading" style="font-size:38px">Camp Chair</div></li>
                 <li class='list'>
-                    <div class='landing_main'>
+                    <div class='main_landing'>
                         <img src='images/chair1.png'/>
-                        
+                        <div class="button_wrapper">
+                    <a href="#">
+                        <div class="button" id="atc">
+                            Add To Wishlist
+                        </div>
+                    </a>
+                </div>           
             </div>
                     <div class='item_list_wrapper'>
                         <div class='content'>
@@ -72,16 +78,37 @@ session_start();
                         <div class="additional_photos">+ 8</div>
                             </div>
                         </div>
+                        <ul class="landing_menu">
+                        <li class="sub_heading"><a href="#description"><span class="media_text" id='landing_selected'>Description&nbsp;&nbsp;</span></a></li>
+                        <li class="sub_heading"><a href="#specifications"><span class="media_text">&nbsp;&nbsp;Specifications</span></a></li>
+                        </ul>   
+                        </li>                        
+                        <li class='landing_desc'><p>Ipsum elit ad sint anim. </span>
+                <span>Velit sint qui ipsum amet ex cupidatat minim non sunt esse enim. </span>
+                <span>Tempor fugiat voluptate eiusmod dolore eu irure elit.</span> 
+                <span>Nostrud adipisicing nulla adipisicing sunt eiusmod occaecat. </span>
+                <span>Consectetur excepteur velit culpa deserunt sit. </span>
+            </p>
+            <p>
+                <span>Reprehenderit ea cillum sit aute fugiat sit minim labore tempor magna amet reprehenderit. </span>
+                <span>Mollit nisi laborum velit pariatur quis aliquip nostrud consectetur pariatur anim amet ipsum sit sit. </span>
+                <span>In eiusmod reprehenderit ipsum fugiat. </span>
+                <span>Laboris elit ut et ullamco esse et voluptate esse eu. </span>
+                <span>Adipisicing deserunt eu id voluptate sint aliqua reprehenderit aliquip aute culpa. </span>
+                <span>Ea aliqua adipisicing aute esse nulla esse cupidatat nostrud pariatur qui ex. </span>
+                <span>Proident sunt dolore non id voluptate. </span>
+                <span>Cillum do in tempor veniam reprehenderit excepteur ipsum pariatur excepteur.   
                         </li>
             </div>        
                 <ul class="item_list" id="cart_total">
-                    <li><div class="sub_heading" style="font-size:38px">Cart Totals</div></li>
-                    <li id="item_total"><b>Total Number of Items: 1</b></li>
+                    <li><div class="sub_heading" style="font-size:25px">Details</div></li>
+                    <li id="item_total"><img src='images/profile.jpg'/><b>John Citizen</b></li>
+                    <li id="estimate">SENIOR member since 2022. 10 Ads.</li>
                     <li><div class="list_of_items"> 
                         Item 1 $$$</br>                     
             </div>
             </li>
-                    <li id="total"><b>Estimated Total: $$$</b></li>
+                    <li id="total" style="font-size:35px"><b>$$$</b></li>
                     <li id="estimate">NOTE: This is not the final total, this is an estimate.</li>
                     <br/>
                     <li><a href="checkout.php"><div class="button" id="checkout">CHECKOUT</div></li>
